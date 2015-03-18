75dd34163c16b24de45bcf9b34fcb046b2c98d3f
Bryan <bryanjappleby@gmail.com>
2015-03-18 14:49:37 +0200

    Fix request data collection

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var receiver = require('./receiver');
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+app.set('port', (process.env.PORT || 5000));
 // app.get('/', function(request, response) {
 	// response.sendFile(__dirname + '/client/views/index.html');
 // });
 
 app.post('/receiver.html', function(request, response) {
-	receiver.handle(request.body);
-	response.send("ok");
+    var body = '';
+    request.on('data', function(data) {
+        body += data;
+    });
+    request.on('end', function (){
+		receiver.handle(body);
+    });
+});
+
+app.post('/receive', function(request, respond) {
 });
 
 app.get('/data.html', function(request, response) {
 	response.sendFile(__dirname + '/data.json');
-
 });
 
 app.listen(app.get('port'), function() {
