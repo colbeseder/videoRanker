d400e3c5437e13b7dc8660ed6b8906ca9f3f5429
Bryan <bryanjappleby@gmail.com>
2015-03-18 02:20:00 +0200

    Back-end skeleton

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var receiver = require('receiver');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -7,6 +8,12 @@ app.use(express.static(__dirname + '/public'));
 // app.get('/', function(request, response) {
 	// response.sendFile(__dirname + '/client/views/index.html');
 // });
+
+app.get('receiver.html', function(request, response) {
+	receiver.handle("---DATA---");
+	response.send("ok");
+});
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
