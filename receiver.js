57eaeb82c8f11e296137d92a39d08d611a114684
Bryan <bryanjappleby@gmail.com>
2015-03-18 02:21:49 +0200

    Back-end skeleton pt2

---
+++ receiver.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+
+module.exports = {
+	handle: function(data){
+		fs.writeFile(__dirname + "/data.json", data, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+
+			console.log("The file was saved!");
+		}); 		
+	}
+};
