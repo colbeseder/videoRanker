32fceb3b4e1fdb8829ec28f941e5f38fb166fba2
Bryan <bryanjappleby@gmail.com>
2015-03-24 23:14:49 +0200

    Rnamed results table to 'results'

--- crud.js
+++ crud.js
@@ -4,7 +4,7 @@ function add() {}
 
 function dump() {
 	pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-		client.query('SELECT * FROM test_table', function (err, result) {
+		client.query('SELECT * FROM results', function (err, result) {
 			done();
 			if (err) {
 				console.error(err);
